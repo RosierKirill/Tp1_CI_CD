2c824b8049d9d33fababd000f1968a601f95489f
kirill <kirill.rosier@lyceeastier.com>
2026-03-20 18:20:18 +0100

    Fix: Corriger le projet

--- src/main/java/com/tp1cicd/StudentController.java
+++ src/main/java/com/tp1cicd/StudentController.java
@@ -1,7 +1,6 @@
 package com.tp1cicd;
 
 import jakarta.validation.Valid;
-import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
