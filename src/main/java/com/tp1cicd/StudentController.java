97452c4c079be94aa19c6d199ea7911225445958
kirill <kirill.rosier@lyceeastier.com>
2026-03-20 18:19:01 +0100

    WIP: Casser le projet - ajout import inutilisé

--- src/main/java/com/tp1cicd/StudentController.java
+++ src/main/java/com/tp1cicd/StudentController.java
@@ -1,6 +1,7 @@
 package com.tp1cicd;
 
 import jakarta.validation.Valid;
+import java.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
